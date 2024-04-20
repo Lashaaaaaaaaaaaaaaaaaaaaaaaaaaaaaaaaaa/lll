985552bb3a49277d9a619e4406cbac0a1333066e
Flacko <141658239+Lashaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa@users.noreply.github.com>
2024-04-21 00:23:50 +0400

    Add files via upload

---
+++ product/bags/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    color = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='assets/')
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='assets/')
+
+    def __str__(self):
+        return self.name
